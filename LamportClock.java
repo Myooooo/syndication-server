d678ce8f44519ce28d89a64ed8726fc88401d1ac
Myooooo <46144728+Myooooo@users.noreply.github.com>
2023-03-19 05:29:59 +1100

    Update LamportClock.java

--- LamportClock.java
+++ LamportClock.java
@@ -1,11 +1,6 @@
 /** 
-* Distributed Systems Assignment 2 2021
-* 
 * LamportClock.java
 * This file implements the Lamport Clock instance
-*  
-* @author Moyang Feng a1726464
-* @date 04/10/2021
 */
 
 package ds.assignment2;
